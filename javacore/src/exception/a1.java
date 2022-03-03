243a74ae89df26a46a880d351dcbd18303e61494
shivansh85 <shivansh.kumar7@gmail.com>
2022-03-03 13:52:01 +0530

    first commit

---
+++ javacore/src/exception/a1.java
@@ -0,0 +1,28 @@
+package exception;
+
+import java.util.Scanner;
+
+public class a1 {
+
+    public static void main(String[] args) {
+        Scanner scanner = null;
+        try {
+            scanner = new Scanner(System.in);
+            System.out.println("Enter Dividend number:");
+            int dividend = scanner.nextInt();
+            System.out.println("Enter Divisor number:");
+            int divisor = scanner.nextInt();
+            int result = dividend/divisor;
+            System.out.println("Result is:"+result);
+        } catch (ArithmeticException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+        finally {
+            if(scanner != null)
+                scanner.close();
+        }
+
+    }
+
+}
