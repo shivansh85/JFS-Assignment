c53a089eb5e39068f3ef7124669b6ebccd334dcd
shivansh85 <shivansh.kumar7@gmail.com>
2022-03-03 14:25:43 +0530

    First commit

--- javacore/src/Data_structure/a1.java
+++ javacore/src/Data_structure/a1.java
@@ -1,5 +1,27 @@
 package Data_structure;
+import java.util.Scanner;
+class ArmstrongOrNot {
+    public static boolean armstrongCheck(int num) {
+        int b,c,d=0 , temp ;
+        temp = num ;
+        while(temp>0){
+            b = temp %10 ;
+            c = b * b * b ;
+            d = c + d ;
+            temp = temp /10;
+        }
+        return d == num;
+    }
+public static class a1 {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter the number-");
+        int num=sc.nextInt();
+        if(ArmstrongOrNot.armstrongCheck(num))
+            System.out.print("the given number "+num+ " is an arm strong number");
+        else System.out.print("the given number "+num+ " is not an arm strong number");
+    }
 
-public class a1 {
+}
 
 }
