d96d1c1b03cf32c25e26c5a5884ca816a497e445
shivansh85 <shivansh.kumar7@gmail.com>
2022-03-03 16:50:19 +0530

    collection framework 1

---
+++ javacore/src/collection_framework_1/a4.java
@@ -0,0 +1,27 @@
+package collection_framework_1;
+
+import java.util.ListIterator;
+import java.io.*;
+import java.util.ArrayList;
+
+public class a4 {
+    public static void main(String[] args)
+    {
+        ArrayList<Integer> List = new ArrayList<Integer>();
+
+        List.add(1);
+        List.add(2);
+        List.add(3);
+        List.add(4);
+        List.add(5);
+
+        ListIterator<Integer> List_Iterator
+                = List.listIterator(List.size());
+
+        System.out.println("Reversed : ");
+
+        while (List_Iterator.hasPrevious()) {
+            System.out.println(List_Iterator.previous());
+        }
+    }
+}
