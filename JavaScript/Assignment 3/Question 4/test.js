261f9b101ff4450402435094bd27757b139a9bbf
shivansh85 <86680958+shivansh85@users.noreply.github.com>
2022-04-05 21:15:18 +0530

    Javascript 3

---
+++ JavaScript/Assignment 3/Question 4/test.js
@@ -0,0 +1,9 @@
+var personName = {
+    firstName : "",
+    lastName : ""
+}
+
+personName.firstName = "shivansh";
+personName.lastName = "kumar";
+console.log(personName.firstName);
+console.log(personName.lastName);
---
+++ JavaScript/Assignment 3/Question 5/test.js
@@ -0,0 +1,14 @@
+var personName = {
+    firstName : "",
+    lastName : ""
+}
+
+personName.firstName = "shivansh";
+personName.lastName = "kumar";
+console.log(personName.firstName);
+console.log(personName.lastName);
+
+//new Question5
+console.log(personName.middleName)
+personName.middleName = "upadhyay";
+console.log(personName.middleName)
